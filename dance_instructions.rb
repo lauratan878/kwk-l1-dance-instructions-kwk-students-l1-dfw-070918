7a2389946ed350e7789cfb723e9eb37cf9e60a28
lauratan878 <lauratan878@gmail.com>
2018-07-10 14:40:20 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,33 @@
+# def lower body
+#   puts "plants legs"
+#   puts "lift right foot"
+#   puts "return right foot"
+#   puts "small skip-step backward"
+# end
+
+# def upper body
+#   puts "hold arms at chest level"
+#   puts "cross right wrist over left wrist and hold together"
+#   puts "lift arms in a loose bounce"
+# end
+
+def starting_stance
+  puts "Plant legs far apart"
+end
+
+def skip_step(left)
+  puts "Lower"
+  puts "Bounce"
+  puts "Briefly"
+end
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot"
+end
+
+def bob the reins
+  puts "bob it out"
+end 
 
 # Code your methods below
